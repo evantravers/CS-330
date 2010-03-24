2208300e1a3969f3d4f33ad07575d7263e75d9a5
Evan Travers <widgetwebworks@gmail.com>
2010-03-24 13:15:56 -0500

    started working on the project

--- assignment2/windows32/hw2.asm
+++ assignment2/windows32/hw2.asm
@@ -9,11 +9,22 @@ INCLUDE io.h                             ; header file for input/output
 .STACK 4096
 
 .DATA
-
+     prompt1   BYTE    "Enter a number: ", 0
+     string    BYTE    40 DUP (?)
+     number1   DWORD   ?
 
 .CODE
 _MainProc PROC
-     ; Problem: Compute the average of numbers, possible number of inputs going up to 100.
-     ; Solution: As each one comes in, increment a counter, and keep adding the numbers together. At the end, divide the sum of all the numbers by the counter, output result.        
+     ; Problem: Compute the average of numbers,
+     ; possible number of inputs going up to 100.
+     ; Solution: As each one comes in, increment a counter, 
+     ; and keep adding the numbers together.
+     ; At the end, divide the sum of all the 
+     ; numbers by the counter, output result.   
+inputLoop:
+     ; for each input
+     input     prompt1, string, 40       ; read ASCII characters
+     atod      string                    ; convert to integer
+     mov       number1, eax              ; move the new number to eax
 _MainProc ENDP
 END                                      ; end of source code
